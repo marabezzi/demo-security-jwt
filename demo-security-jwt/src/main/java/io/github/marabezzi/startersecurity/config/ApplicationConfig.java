1da8b67bf6c8e54a0d0080070e363d59014868b2
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2023-12-01 14:04:27 -0300

    criar authetication manager bean

--- demo-security-jwt/src/main/java/io/github/marabezzi/startersecurity/config/ApplicationConfig.java
+++ demo-security-jwt/src/main/java/io/github/marabezzi/startersecurity/config/ApplicationConfig.java
@@ -2,9 +2,15 @@ package io.github.marabezzi.startersecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +23,25 @@ public class ApplicationConfig {
 		return username -> repository.findByEmail(username)
 				.orElseThrow(() -> new UsernameNotFoundexception("Usuário não encontrado!"));
 }
+	
+	@Bean
+	public AuthenticationProvider authticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+		return config.getAuthenticationManager();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
 
 
